8618b075f202e7286cfe033a2db19d61bee2089a
Kartik Sharma <98kartik.sharma@gmail.com>
2017-03-27 16:05:25 +0530

    Update intern_eat_json.php

--- intern_eat_json.php
+++ intern_eat_json.php
@@ -9,7 +9,7 @@ while($row =mysqli_fetch_assoc($result))
 {
 	$emparray[] = $row;
 }
-$final=array("places"=>$emparray);
+$final=array("eat"=>$emparray);
 //echo json_encode($final,64);
 echo json_encode($final,JSON_UNESCAPED_SLASHES);
 
